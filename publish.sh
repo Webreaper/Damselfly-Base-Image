d5f798d1a1dc47d5abf888f61eeb2703d20fd012
Mark Otway <mark@otway.com>
2021-07-15 23:39:22 +0100

    Try without single-file

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 export MSBuildEnableWorkloadResolver=false
-dotnet publish EmguCVSample.sln -r linux-x64 -c Release --self-contained true /p:PublishSingleFile=true /p:Version=1.0.0 /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish EmguCVSample.sln -r linux-x64 -c Release /p:Version=1.0.0 # --self-contained true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
 docker buildx build --platform linux/amd64 --push -t webreaper/emgusample:dev .
 
