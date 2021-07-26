2fc36d1220ede0e53d381cc6f4f3064ad3defc15
Mark Otway <mark@otway.com>
2021-07-26 10:11:42 +0100

    dotnet run vs run

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 set -e
 
 # echo "Increasing inotify watch limit..."
-# echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+# echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p    
+
+echo "Dotnet Run:"
+dotnet run
 
 cd /emgu/bin/Release/net5.0/linux-x64/
+echo "Run emgu in linux-x64"
 ./emgu
+echo "Run emgu.dll in linux-x64"
 ./emgu.dll
