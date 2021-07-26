a49171bc2dfcfbe6246fc391b3a1a804f97accd7
Mark Otway <mark@otway.com>
2021-07-26 10:24:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ set -e
 echo "Dotnet Run:"
 dotnet run
 
-cd /emgu/bin/Release/net5.0/linux-x64/
-echo "Run emgu in linux-x64"
-./emgu
-echo "Run emgu.dll in linux-x64"
-./emgu.dll
+echo "Run Compiled:"
+./emgu/bin/Release/net5.0/linux-x64/emgu
+
+echo "Run Published:"
+./emgu/bin/Release/net5.0/linux-x64/publish/emgu
