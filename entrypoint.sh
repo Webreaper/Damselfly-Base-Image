15eb79c5d6abc37a404942433fa1f1db10ed5684
Mark Otway <mark@otway.com>
2021-07-26 10:10:19 +0100

    Build, don't publish

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 # echo "Increasing inotify watch limit..."
 # echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
-cd /emgu/bin/Release/net5.0/linux-x64/publish/
+cd /emgu/bin/Release/net5.0/linux-x64/
 ./emgu
+./emgu.dll
