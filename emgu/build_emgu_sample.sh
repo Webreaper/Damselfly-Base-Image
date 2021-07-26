420873bfb20e25a8b9a6c544b185cbf649150649
Mark Otway <mark@otway.com>
2021-07-26 14:01:26 +0100

    Manually download and install libcvextern.so

--- emgu/build_emgu_sample.sh
+++ emgu/build_emgu_sample.sh
@@ -5,10 +5,9 @@ dotnet new console
 dotnet restore
 dotnet publish emgu.csproj -r linux-x64 -f net5.0 -c Release /p:Version=1.0.0 
 
-echo "Build complete. Publish folder contents:"
-ls /emgu/bin/Release/net5.0/linux-x64/publish
+echo "Build complete. Installing libcvextern.so...."
+wget https://www.nuget.org/api/v2/package/Emgu.CV.runtime.ubuntu.20.04-x64/4.5.1.4349
+unzip 4.5.1.4349
+cp runtimes/ubuntu.20.04-x64/native/libcvextern.so /emgu/bin/Release/net5.0/linux-x64/publish
 
-echo "Unzipping and copying libcvextern"
-unzip libcvextern.zip
-cp libcvextern.so /emgu/bin/Release/net5.0/linux-x64/publish
 ls /emgu/bin/Release/net5.0/linux-x64/publish
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 # echo "Increasing inotify watch limit..."
 # echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p    
 
-echo "Dotnet Run:"
-dotnet run
-
 echo "Publish Run"
 /emgu/bin/Release/net5.0/linux-x64/publish/emgu
 
