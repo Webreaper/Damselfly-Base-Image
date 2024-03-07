5c708294bb67f986de6c41319fd1d7811eca265f
Mark Otway <mark@otway.com>
2024-03-07 13:29:37 +0000

    Remove comment

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,6 @@
 
 echo "=== Starting Cleanup"
 
-Disable this stuff for now, as it breaks EMGUCV. Revisit once we've rewritten the AI stuff.
 apt-get update
 apt-get remove -y build-essential git curl make perl
 apt-get autoremove -y
